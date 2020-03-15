e87764b0841c2e38cf211097c5b5586584249b4f
Teodor G <grigor.teodor@gmail.com>
2020-03-15 16:49:24 +0000

    created timer elements container

--- src/app/game/options/GameOptions.java
+++ src/app/game/options/GameOptions.java
@@ -11,13 +11,14 @@ public class GameOptions
 
     private JPanel gameOptions;
     private MaterialElements materialElements;
+    private MaterialLabel timerMinutes, timerSeconds, timerMiliseconds;
 
     public GameOptions()
     {
 
         materialElements = new MaterialElements();
         gameOptions = new JPanel();
-//        gameOptions.setLayout(new FlowLayout());
+        gameOptions.setLayout(new FlowLayout());
         gameOptions.setBackground(new Color(238, 241, 93));
 //        gameOptions.setBackground(new Color(241, 241, 241));
         gameOptions.setPreferredSize(new Dimension(255, 585));
@@ -30,11 +31,41 @@ public class GameOptions
 
         JPanel timerLabelHolder = new JPanel();
         timerLabelHolder.setBackground(new Color(0, 0, 0, 0));
+        timerLabelHolder.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
         timerLabelHolder.setLayout(new BorderLayout());
+
         MaterialLabel timerLabel = materialElements.createLabel("DIGITAL TIMER");
         timerLabelHolder.add(timerLabel, BorderLayout.CENTER);
         gameOptions.add(timerLabelHolder);
 
+        JPanel timerHolder = new JPanel();
+        timerHolder.setBackground(new Color(0, 0, 0, 0));
+        timerHolder.setPreferredSize(new Dimension(255, 15));
+        timerHolder.setBorder(BorderFactory.createEmptyBorder(-5, 0, 0, 0));
+
+
+        timerMinutes = createTimerElement("00");
+        timerSeconds = createTimerElement("00");
+        timerMiliseconds = createTimerElement("00");
+
+        timerHolder.add(timerMinutes);
+        timerHolder.add(new JLabel(" : "));
+        timerHolder.add(timerSeconds);
+        timerHolder.add(new JLabel(" : "));
+        timerHolder.add(timerMiliseconds);
+
+        gameOptions.add(timerHolder);
+
+    }
+
+    public MaterialLabel createTimerElement(String text)
+    {
+        MaterialLabel materialLabel = materialElements.createLabel(text);
+        materialLabel.setBorder(BorderFactory.createEmptyBorder(0, 3, 0, 3));
+        materialLabel.setOpaque(true);
+        materialLabel.setBackground(Color.black);
+        materialLabel.setForeground(Color.white);
+        return materialLabel;
     }
 
     public JPanel getGameOptionsContainer()
