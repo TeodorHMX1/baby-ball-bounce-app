455d3f0d53bb4bac192e302ebfb8a03c73041a9d
Teodor G <grigor.teodor@gmail.com>
2020-03-15 20:24:52 +0000

    initialize players

--- src/app/models/Player.java
+++ src/app/models/Player.java
@@ -1,19 +1,22 @@
 package app.models;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Player
 {
 
-    private Image babyImage;
+    private ImageIcon babyImage;
     private int x, y;
     private int team;
     private int id;
 
-    public Player(int x, int y, Image image, int team, int id)
+    public Player(int x, int y, ImageIcon image, int team, int id)
     {
         this.x = x;
         this.y = y;
+        this.babyImage = image;
+        this.id = id;
         this.team = team;
     }
 
--- src/app/utils/AppUtils.java
+++ src/app/utils/AppUtils.java
@@ -1,8 +1,12 @@
 package app.utils;
 
 import app.interfaces.GameTimer;
+import app.models.Player;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppUtils
 {
@@ -16,6 +20,7 @@ public class AppUtils
     public static void setMainActivity(JFrame mainActivity)
     {
         mainActivityGlobal = mainActivity;
+        initializePlayers();
     }
     public static JFrame getAppWindow() {
         return mainActivityGlobal;
@@ -31,7 +36,37 @@ public class AppUtils
         return players;
     }
 
+    private static List<Player> mPlayers = new ArrayList<>();
+    public static void initializePlayers()
+    {
+        ImageIcon imageIcon1 = new ImageIcon("assets/images/baby1.png");
+        Image image1 = imageIcon1.getImage();
+        Image newimg1 = image1.getScaledInstance(31, 31, Image.SCALE_SMOOTH);
+        imageIcon1 = new ImageIcon(newimg1);
 
+        ImageIcon imageIcon2 = new ImageIcon("assets/images/baby2.png");
+        Image image2 = imageIcon2.getImage();
+        Image newimg2 = image2.getScaledInstance(31, 31, Image.SCALE_SMOOTH);
+        imageIcon2 = new ImageIcon(newimg2);
+
+        mPlayers.clear();
+        Player mPlayer1 = new Player(6, 4, imageIcon1, 1, 1);
+        mPlayers.add(mPlayer1);
+        Player mPlayer2 = new Player(6, 11, imageIcon2, 2, 2);
+        mPlayers.add(mPlayer2);
+        Player mPlayer3 = new Player(6, 4, imageIcon1, 1, 3);
+        mPlayers.add(mPlayer3);
+        Player mPlayer4 = new Player(6, 11, imageIcon2, 2, 4);
+        mPlayers.add(mPlayer4);
+    }
+    public static Player getPlayer(int player)
+    {
+        return mPlayers.get(player - 1);
+    }
+    public static List<Player> getPlayer()
+    {
+        return mPlayers;
+    }
 
     private static int seconds = 0;
     public static void increaseSeconds()
