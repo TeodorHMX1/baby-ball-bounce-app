37bb775db86506743c4934c6fcc5f2d6b6b1ce23
Teodor G <grigor.teodor@gmail.com>
2020-03-15 14:16:21 +0000

    create environment

--- src/app/game/world/GameWorld.java
+++ src/app/game/world/GameWorld.java
@@ -1,10 +1,17 @@
 package app.game.world;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GameWorld
 {
 
     public GameWorld()
     {
+
+        JPanel gameWorld = new JPanel();
+        gameWorld.setBorder(BorderFactory.createEmptyBorder(30, 20, 30, 20));
+        gameWorld.setBackground(Color.BLUE);
 
     }
 
