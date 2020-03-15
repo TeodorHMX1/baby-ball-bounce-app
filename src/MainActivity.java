e49b57a90e8b29eccf01e293c3eac2ee1d886472
Teodor G <grigor.teodor@gmail.com>
2020-03-15 13:12:39 +0000

    craeted main activity

---
+++ src/MainActivity.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class MainActivity extends JFrame {
+
+    static MainActivity activity;
+
+    public static void main(String[] args)
+    {
+
+        // TODO Auto-generated method stub
+        activity = new MainActivity();
+        activity.setTitle("CBabyBallBounce – Baby Ball Bounce Application");
+        activity.setSize(825, 585);
+        activity.setResizable(false);
+        activity.setVisible(true);
+
+    }
+
+}
