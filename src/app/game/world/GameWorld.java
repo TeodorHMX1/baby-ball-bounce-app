0a43864e72ad40c2b6687176e5a418cd61d16045
Teodor G <grigor.teodor@gmail.com>
2020-03-15 14:34:20 +0000

    set game holder content

--- src/app/game/world/GameWorld.java
+++ src/app/game/world/GameWorld.java
@@ -12,8 +12,21 @@ public class GameWorld
     {
 
         gameWorld = new JPanel();
-        gameWorld.setBackground(Color.BLUE);
+        gameWorld.setBorder(BorderFactory.createEmptyBorder(30, 20, 30, 20));
         gameWorld.setPreferredSize(new Dimension(570, 440));
+        gameWorld.setBackground(new Color(241, 241, 241));
+
+        initializeGameSubHolder();
+
+    }
+
+    private void initializeGameSubHolder() {
+
+        JPanel field = new JPanel();
+        field.setPreferredSize(new Dimension(530, 440));
+        field.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        field.setBackground(Color.white);
+        gameWorld.add(field);
 
     }
 
