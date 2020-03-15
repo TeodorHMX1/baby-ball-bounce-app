8e07dc84bc2fcbba29159fb0772ab8afe28202b7
Teodor G <grigor.teodor@gmail.com>
2020-03-15 16:38:42 +0000

    created timer label

--- src/app/game/options/GameOptions.java
+++ src/app/game/options/GameOptions.java
@@ -1,5 +1,8 @@
 package app.game.options;
 
+import app.utils.material.MaterialElements;
+import app.utils.material.MaterialLabel;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,13 +10,30 @@ public class GameOptions
 {
 
     private JPanel gameOptions;
+    private MaterialElements materialElements;
 
     public GameOptions()
     {
 
+        materialElements = new MaterialElements();
         gameOptions = new JPanel();
-        gameOptions.setBackground(new Color(241, 241, 241));
+//        gameOptions.setLayout(new FlowLayout());
+        gameOptions.setBackground(new Color(238, 241, 93));
+//        gameOptions.setBackground(new Color(241, 241, 241));
         gameOptions.setPreferredSize(new Dimension(255, 585));
+
+        createDigitalTimer();
+
+    }
+
+    private void createDigitalTimer() {
+
+        JPanel timerLabelHolder = new JPanel();
+        timerLabelHolder.setBackground(new Color(0, 0, 0, 0));
+        timerLabelHolder.setLayout(new BorderLayout());
+        MaterialLabel timerLabel = materialElements.createLabel("DIGITAL TIMER");
+        timerLabelHolder.add(timerLabel, BorderLayout.CENTER);
+        gameOptions.add(timerLabelHolder);
 
     }
 
