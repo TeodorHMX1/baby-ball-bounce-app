a7d7039e3fd769404b0c452f0b76e4e19a7de265
Teodor G <grigor.teodor@gmail.com>
2020-03-15 13:58:37 +0000

    make the window to appear in the middle of the screen

--- src/app/MainActivity.java
+++ src/app/MainActivity.java
@@ -41,6 +41,10 @@ public class MainActivity extends JFrame
         //close on 'x' clicked
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        // make the window to appear in the middle of the screen
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+
     }
 
     private void createUIX()
