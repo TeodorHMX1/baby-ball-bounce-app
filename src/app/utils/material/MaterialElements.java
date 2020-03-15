ffad5d3ae008a506a886dd2ca1ac3c0308218472
Teodor G <grigor.teodor@gmail.com>
2020-03-15 15:38:49 +0000

    improved material button style

--- src/app/utils/material/MaterialElements.java
+++ src/app/utils/material/MaterialElements.java
@@ -3,21 +3,27 @@ package app.utils.material;
 import javax.swing.*;
 import java.awt.*;
 
-public class MaterialElements {
+public class MaterialElements
+{
 
     public MaterialElements()
     {
 
     }
 
-    public JButton newButton(Icon icon, String text)
+    public MaterialButton newButton(Icon icon, String text)
     {
-        JButton btn = new JButton(icon);
-        btn.setText(text);
-        btn.setBorder(BorderFactory.createCompoundBorder(
-                BorderFactory.createLineBorder(new Color(0, 0, 0), 1),
-                BorderFactory.createEmptyBorder(3, 8, 3, 8)));
+        MaterialButton btn = new MaterialButton(text);
         btn.setBackground(new Color(255, 255, 255));
+        btn.setHoverBackgroundColor(new Color(243, 243, 243));
+        btn.setPressedBackgroundColor(new Color(230, 230, 230));
+        btn.setIcon(icon);
+        btn.setBorder(
+                BorderFactory.createCompoundBorder(
+                    BorderFactory.createLineBorder(new Color(0, 0, 0), 1),
+                    BorderFactory.createEmptyBorder(3, 8, 3, 8)
+                )
+        );
         return btn;
     }
 }
