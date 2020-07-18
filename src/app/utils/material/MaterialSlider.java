5369758b1b16f41b289fdd18e71cd356d7a8763f
Teodor G <grigor.teodor@gmail.com>
2020-07-18 18:28:57 +0300

    appendix added to the enum

--- src/app/utils/material/MaterialSlider.java
+++ src/app/utils/material/MaterialSlider.java
@@ -6,8 +6,8 @@
  * Course: BSc Computing Year 1
  * Module: CSY1020 Problem Solving & Programming
  * Tutor: Gary Hill
- * @version: 2.6 Ball Direction added
- * Date: 23/06/20
+ * @version: 1.0 File Created
+ * Date: 21/06/20
  */
 package app.utils.material;
 
