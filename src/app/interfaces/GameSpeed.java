ba0fc77b5e0bc4b6e7355bc0a396b57035faf0a6
Teodor G <grigor.teodor@gmail.com>
2020-07-18 18:38:52 +0300

    appendix added to the interfaces

--- src/app/interfaces/GameSpeed.java
+++ src/app/interfaces/GameSpeed.java
@@ -1,5 +1,17 @@
+/**
+ * Program: Assignment 2: Application – Baby Ball Bounce
+ * Filename: GameSpeed.java
+ *
+ * @author: © Teodor Grigor (GitHub - TeodorHMX1)
+ * Course: BSc Computing Year 1
+ * Module: CSY1020 Problem Solving & Programming
+ * Tutor: Gary Hill
+ * @version: 1.0 File created
+ * Date: 18/05/20
+ */
 package app.interfaces;
 
-public interface GameSpeed {
+public interface GameSpeed
+{
     void onSpeedChanged();
 }
--- src/app/interfaces/GameTimer.java
+++ src/app/interfaces/GameTimer.java
@@ -1,8 +1,21 @@
+/**
+ * Program: Assignment 2: Application – Baby Ball Bounce
+ * Filename: GameTimer.java
+ *
+ * @author: © Teodor Grigor (GitHub - TeodorHMX1)
+ * Course: BSc Computing Year 1
+ * Module: CSY1020 Problem Solving & Programming
+ * Tutor: Gary Hill
+ * @version: 1.0 File created
+ * Date: 18/05/20
+ */
 package app.interfaces;
 
-public interface GameTimer {
+public interface GameTimer
+{
 
-    interface GameStarted {
+    interface GameStarted
+    {
         void isRunning(boolean running);
     }
 
